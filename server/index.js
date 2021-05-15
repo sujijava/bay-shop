64a9cb48d556f3f6800fed0ac2c662e2ab2f0497
susieoh8@gmail.com <59837559+sujijava@users.noreply.github.com>
2021-05-15 19:08:35 -0400

    ready for deploy

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
-import express from 'express'
+//production
+/**
+ * import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
@@ -36,6 +38,45 @@ const PORT = process.env.PORT || 5000
 
 mongoose
   .connect(CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port`)))
+  .catch((error) => console.log(`${error} did not connect`))
+
+mongoose.set('useFindAndModify', false)
+**/
+
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import listRoutes from './routes/lists.js'
+import path from 'path'
+
+const app = express()
+const __dirname = path.resolve()
+
+app.use(bodyParser.json({}))
+app.use(bodyParser.urlencoded({}))
+app.use(cors())
+
+app.use('/server/cart', listRoutes)
+
+//Serve static assets if in production
+// if (process.env.NODE_ENV === 'production') {
+//set static folder
+app.use(express.static('client/build'))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
+
+const CONNECTION_URL =
+  process.env.MONGODB_URI ||
+  'mongodb+srv://dbUser:susieoh@cluster0.dhawv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+const PORT = process.env.PORT || 5000
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port`)))
   .catch((error) => console.log(`${error} did not connect`))
 
